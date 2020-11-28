12f7d9e46a247bfe1328a5fc1f5b655c1cfdb755
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2020-11-28 11:17:14 +0100

    Bug in Node stub

--- calendrical-javascript-node.js
+++ calendrical-javascript-node.js
@@ -1,4 +1,4 @@
 /* Node.js entry file for calendrical-javascript package as a module */
-exports.nodeNotification = function {
+exports.nodeNotification = function () {
 	console.log ("calendrical-javascript module identified");
 }
