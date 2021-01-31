7efc0e532704ef7c85b13681504e2e2a8f76d608
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2021-01-31 19:59:03 +0100

    No calendarinstant
    Only the general purpose software is left in the module. Specific instantiation is left in /docs only, as an example.

--- aggregate.js
+++ aggregate.js
@@ -1,7 +1,5 @@
 /* aggregation module for calendrical-javascript: all entries collected and reexported from here */
-// const prefix = "./node_modules/calendrical-javascript/"; // "./node_modules/calendrical-javascript/" or "./"
 export * from "./chronos.js";
 export * from "./dateextended.js";
 export * from "./calendars.js";
-export * from "./calendarinstant.js";
-// You still have to import from local .js file.
+export { default as pldrDOM } from "./pldr.js";
