2a520c5dfa4a8fac491c553043a6535808e1d3e8
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2021-07-16 12:43:34 +0200

    Github page transferred at root

--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# GitHub site for calendrical-javascript
-
-This folder contains the files for the demo site at https://louis-aime.github.io/calendrical-javascript/
--- docs/aggregate.js
+++
@@ -1,7 +0,0 @@
-/* aggregation module for calendrical-javascript: all entries collected and reexported from here */
-export * from "./chronos.js";
-export { default as ExtDate } from "./extdate.js";
-export { default as ExtDateTimeFormat } from "./extdatetimeformat.js";
-export { default as fetchDOM } from "./fetchdom.js";
-export * from "./calendars.js";
-export { default as getPldrDOM } from "./pldr.js";
