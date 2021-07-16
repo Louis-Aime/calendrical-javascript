8dd07daa0c5001d906d581b31e0d4b5ab7f66f2c
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2021-07-16 11:17:08 +0200

    Adapt to new organisation
    Dateextended.js splitted
    pldr.js as a module
    fetchdom.js as a module

--- docs/aggregate.js
+++ docs/aggregate.js
@@ -1,5 +1,7 @@
 /* aggregation module for calendrical-javascript: all entries collected and reexported from here */
 export * from "./chronos.js";
-export * from "./dateextended.js";
+export { default as ExtDate } from "./extdate.js";
+export { default as ExtDateTimeFormat } from "./extdatetimeformat.js";
+export { default as fetchDOM } from "./fetchDOM.js";
 export * from "./calendars.js";
-// export { default as pldrDOM } from "./pldr.js";
+export { default as getPldrDOM } from "./pldr.js";
