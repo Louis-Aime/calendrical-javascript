c958d44e9018230c6ae672b51594864aca869f14
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2021-08-12 16:54:53 +0200

    Add new time-units.js file

--- aggregate.js
+++ aggregate.js
@@ -2,6 +2,7 @@
 export * from "./chronos.js";
 export { default as ExtDate } from "./extdate.js";
 export { default as ExtDateTimeFormat } from "./extdatetimeformat.js";
+export { default as Milliseconds } from "./time-units.js";
 export { default as fetchDOM } from "./fetchdom.js";
 export * from "./calendars.js";
 export { default as getPldrDOM } from "./pldr.js";
